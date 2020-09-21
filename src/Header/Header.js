fe444ebeaf40399035454b38e7adc5e9e6d87a09
masum-webdev <masum.webdev@gmail.com>
2020-09-21 10:59:15 +0600

    navbar added

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav class="navbar navbar-light bg-primary ">
+  <a class="navbar-brand text-white" href="#">Home</a>
+</nav>
+  );
+};
+
+export default Header;
